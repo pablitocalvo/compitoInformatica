637c9f4df6ee849ace948d3b6eae9e3a6a8ff18d
pablitocalvo <sbtpla68@yahoo.it>
2016-11-24 07:00:32 +0100

    primo commit

---
+++ ESE.CPP/01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout<<"PAO_ciao";
+	cout<<" PAO_mondo"<<endl;
+	cout<<PAO_23
+}
+
+#PAO_def 01 PAO_ciao ciao       PAO_mondo  mondo     PAO_23 11;
+#PAO_def 02 PAO_ciao bella      PAO_mondo  mondo     PAO_23 99;
+#PAO_def 03 PAO_ciao hello      PAO_mondo  pluto     PAO_23 43;
+#PAO_def 04 PAO_ciao ciao  	    PAO_mondo  ragazzi   PAO_23 24;
+#PAO_def 05 PAO_ciao pischelli  PAO_mondo  ciao      PAO_23 65;
+#PAO_def 06 PAO_ciao hasta      PAO_mondo  lavista   PAO_23 76;
